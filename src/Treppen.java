0711217fe2c41fba33a549b5eb2d9af289f4680c
Marlon Adam <marlon.adam@charite.de>
2023-01-11 08:16:17 +0100

    z hinzugefügt

---
+++ src/Treppen.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Treppen
+{
+        public static void main(String[] args)
+        {
+            Scanner inhöhe = new Scanner(System.in);
+            System.out.println("Treppenhöhe: ");
+            int hohe = inhöhe.nextInt();
+
+            Scanner inbreite = new Scanner(System.in);
+            System.out.println("Stufenbreite: ");
+            int breite = inbreite.nextInt();
+
+            String a = "*";
+            String b = "";
+
+            //int z = breite * hohe;
+
+            for(int i = 0; i < breite; i++){
+                b = b+a;
+            }
+
+            String c = "";
+            String d = "";
+            for(int i = 0; i < hohe; i++){
+                c = c + b;
+                //z -= 2;
+                // z Leerzeichen müssen vor c
+                System.out.println(c);
+            }
+        }
+}
+
